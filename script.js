f3e8c05f9848cb9dbedc1b4574775a080a34475e
d1scipline <143661704+d1scipline@users.noreply.github.com>
2025-10-06 23:16:06 +0300

    add createGameboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+function createGameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) board[i].push("");
+  }
+
+  function getBoard() {
+    return board;
+  }
+
+  function mark(y, x, playerSign) {
+    board[y][x] = playerSign;
+  }
+
+  //Wil be deleted
+  function printBoard() {
+    console.log(board);
+  }
+
+  return { getBoard, mark, printBoard };
+}
